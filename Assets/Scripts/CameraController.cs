d41e81e1f9cdab66267bdb6803b745547c34ada2
James Huynh <james.huynh@mail.mcgill.ca>
2020-01-11 14:37:31 -0500

    finished basic menu

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject character;
+    public Vector3 charPos;
+
+    void Start() {
+        charPos = character.transform.position;
+    }
+
+    void LateUpdate() {
+
+        this.transform.position +=  character.transform.position - charPos;
+        charPos = character.transform.position;
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public Animator animator;
+
+    public GameObject detectionCube;
+    public float spawnTime;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    public float speed;
+    Vector3 movement;
+    int direction; // 0 = down, 1 = up, 2 = left, 3 = right
+
+    void Start() {
+        movement = new Vector3(0, -speed, 0);
+        direction = 0;
+        spawnTime = 0;
+    }
+
+    void Update() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        if (direction == 0 || direction == 1) {
+            if (verticalInput != 0) {
+                horizontalInput = 0;
+            }
+        }
+        else {
+            if (horizontalInput != 0) {
+                verticalInput = 0;
+            }
+        }
+
+        animator.SetFloat("HorizontalInput", horizontalInput);
+        animator.SetFloat("VerticalInput", verticalInput);
+
+        // change direction
+        switch (direction) {
+            case 0:
+                if (horizontalInput < 0) {
+                    direction = 2;
+                } else if (horizontalInput > 0) {
+                    direction = 3;
+                }
+                break;
+            case 1:
+                if (horizontalInput < 0) {
+                    direction = 2;
+                } else if (horizontalInput > 0) {
+                    direction = 3;
+                }
+                break;
+            case 2:
+                if (verticalInput < 0) {
+                    direction = 0;
+                } else if (verticalInput > 0) {
+                    direction = 1;
+                }
+                break;
+            case 3:
+                if (verticalInput < 0) {
+                    direction = 0;
+                } else if (verticalInput > 0) {
+                    direction = 1;
+                }
+                break;
+        }
+
+        // change position
+        switch (direction) {
+            case 0:
+                movement = new Vector3(0, -speed * Time.deltaTime, 0);
+                transform.position = transform.position + movement;
+                break;
+            case 1:
+                movement = new Vector3(0, speed * Time.deltaTime, 0);
+                transform.position = transform.position + movement;
+                break;
+            case 2:
+                movement = new Vector3(-speed * Time.deltaTime, 0, 0);
+                transform.position = transform.position + movement;
+                break;
+            case 3:
+                movement = new Vector3(speed * Time.deltaTime, 0, 0);
+                transform.position = transform.position + movement;
+                break;
+        }
+
+        // detection of trail
+        spawnTime += Time.deltaTime;
+        if (spawnTime >= speed/10.0) {
+            spawnTime = 0;
+            StartCoroutine(spawnCube(this.transform.position));
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("collision");
+    }
+
+    public IEnumerator spawnCube(Vector3 pos) {
+        yield return new WaitForSeconds(0.5f);
+        GameObject newObject = Instantiate(detectionCube, pos, Quaternion.identity);
+        Destroy(newObject, 6.0f);
+    }
+    
+}
